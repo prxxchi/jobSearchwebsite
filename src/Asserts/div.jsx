461b307cfe058e69d3dbdc9888fb458edda2e46d
prxxchi <108013565+prxxchi@users.noreply.github.com>
2024-02-16 14:16:08 +0530

    Create div.jsx

---
+++ src/Asserts/div.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/FooterDiv/Footer'
+import Job from './Components/JobDiv/Job'
+import Search from './Components/SearchDiv/Search'
+import Value from './Components/ValueDiv/Value'
+
+
+
+function App() {
+  return (
+    <div className='w-[85%] m-auto bg-white'>
+      <NavBar/>
+      <Search/>
+      <Job/>
+      <Value/>
+      <Footer/>
+      
+    
+
+     
+
+    
+    
+    </div>
+  )
+}
+
+export default App
